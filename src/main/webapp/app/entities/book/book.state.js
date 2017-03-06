d5436a38a55ce3b54499ebdffc8f23caa9440dad
Pierre-Yves Beaudouin <pybeaudouin@users.noreply.github.com>
2017-03-05 22:58:50 -0500

    Book list: default sorting by title

--- src/main/webapp/app/entities/book/book.state.js
+++ src/main/webapp/app/entities/book/book.state.js
@@ -29,7 +29,7 @@
                     squash: true
                 },
                 sort: {
-                    value: 'id,asc',
+                    value: 'title,asc',
                     squash: true
                 },
                 search: null
